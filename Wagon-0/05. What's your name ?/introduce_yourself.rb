3d56964aba70d3acbe1ae7605a6f0e71a6c30208
Mathieu BERNARD <mathieubernard@MacBook-de-Mathieu-BERNARD.local>
2014-01-06 19:31:58 +0100

    solution ruby-0-ex5

--- Wagon-0/05. What's your name ?/introduce_yourself.rb
+++ Wagon-0/05. What's your name ?/introduce_yourself.rb
@@ -1,1 +1,12 @@
 # your code goes here, introduce yourself !
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your middle name?"
+middle_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts "Hello, #{first_name} #{middle_name} #{last_name}"
+puts "Bonjour" + " " + first_name + " " + middle_name + " " + last_name
--- Wagon-0/06. Stupid coaching/stupid_coaching.rb
+++ Wagon-0/06. Stupid coaching/stupid_coaching.rb
@@ -1,5 +1,15 @@
 def stupid_coaching 
   # your code goes here
+  puts "What are you doing?"
+  answer = gets.chomp.downcase
+  until answer.include? "i am going to work right now sir !"
+    if answer.include? ""
+    else
+    end
+    puts "what else?"
+    answer = gets.chomp.downcase
+  end
+  puts "ouais bien fait!!"
 end
 
 # Calling the method
