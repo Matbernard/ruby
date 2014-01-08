240fc697d9a76b33e529a0811c8a88c31cd8a653
Mathieu BERNARD <mathieubernard@MacBook-de-Mathieu-BERNARD.local>
2014-01-08 14:39:49 +0100

    codestyle

--- Wagon-1/01. Simple looping/simple_looping.rb
+++ Wagon-1/01. Simple looping/simple_looping.rb
@@ -1,10 +1,29 @@
 # The objective is to create a function that computes the sum of the integers from a min value to a max value
+# y = a + b & a et b sont compris entre un min (0) et un max (100) &  a et b sont des entiers.
+# puts un entier entre 1 et 100
 
 def sum(min,max)
   # your code here
+  res = 0
+  n = min
+
+  while n <= max
+  	res += n
+  	n += 1
+  end
+
+  res
 end
 
-# Testing your code
+def sum(min, max)
+	res = 0
+	
+	for n in min..max
+		res += n
+	end
+	
+	res
+end
 
 min = 1
 max = 100
