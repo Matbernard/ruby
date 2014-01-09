752643f44f1f7dba9851e7ca4a2e1dcad9af2300
Your Name <you@example.com>
2014-01-09 17:42:29 +0100

    exo0-04 et exo1-09

--- Wagon-0/04. Age in days/age_in_days.rb
+++ Wagon-0/04. Age in days/age_in_days.rb
@@ -1,4 +1,4 @@
-def age_in_days(day, month, year)
+def age_in_days(year, month, day)
   # your code goes here !
   t1 = Time.now.to_i
   t2 = Time.new(year, month, day).to_i
@@ -7,4 +7,4 @@ end
 
 
 # Testing your code
-puts age_in_days(25, 07, 1978)
+puts age_in_days(1984, 8, 30)
--- Wagon-1/09. Debug Gym/bugged_concatenate.rb
+++ Wagon-1/09. Debug Gym/bugged_concatenate.rb
@@ -1,8 +1,8 @@
 def bugged_concatenate(array)
   # 2. fix the code: TypeError
-  puts array.inject("") { |output, element| output + element }.upcase
+  puts array.inject("I love ") { |output, element| output + element.to_s }
 end
 
 
 # 1. fix the method call: ArgumentError
-bugged_concatenate( 1, "9", 84, " ", "George Orwell" )
+bugged_concatenate([1, "9", 84, " ", "George Orwell"])
